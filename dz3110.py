e59d43a157be5f99c580d4b15a23db23cf3d79ab
DBystrova94 <117100176+DBystrova94@users.noreply.github.com>
2022-10-31 18:21:22 +0300

    dz3110
    dz3110

---
+++ dz3110.py
@@ -0,0 +1,19 @@
+def awesome_func():
+  x = 5
+  while x != 0:
+    y = 0
+    y = y + x
+    x = x - 1
+  return (y)
+
+
+def awesome_func1():
+  y = 3
+  while y != 5:
+    y += 1
+  return (y)
+
+
+awesome_func()  #Вызов функции
+#awesome_func1()
+print(awesome_func())
