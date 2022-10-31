27f5d889e7cd769ead782b1badb00b20cb3eda58
Alex <nekhorosheff.a.v@yandex.ru>
2022-10-31 20:41:39 +0300

    add code for automation qa

--- dz3110.py
+++ dz3110.py
@@ -13,7 +13,24 @@ def awesome_func1():
     y += 1
   return (y)
 
-
+#
 awesome_func()  #Вызов функции
 #awesome_func1()
 print(awesome_func())
+
+
+# by Alex
+class TestElements:
+  class TestTextBox:
+
+    def test_text_box(self, driver):
+      text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
+      text_box_page.open()
+      text_box_page.fill_all_fields()
+      output_name, output_email, output_current, output_permanent = text_box_page.check_field_form()
+      print(output_name)
+      print(output_email)
+      print(output_current)
+      print(output_permanent)
+      time.sleep(5)
+
